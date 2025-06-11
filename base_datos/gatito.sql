a52cabe425691ba8ef0e935e86cfcdae9f5a12ce
Ramses Alejandro <ram.alex1211@gmail.com>
2025-06-11 09:30:54 -0600

    mi primer commit

---
+++ base_datos/gatito.sql
@@ -0,0 +1,12 @@
+CREATE TABLE estudiantes(
+    id int(11) NOT NULL,
+    nombre varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO estudiantes(id, nombre) VALUES (435, 'Juan Ramirez');
+INSERT INTO estudiantes(id, nombre) VALUES (552, 'Ana Perez');
+INSERT INTO estudiantes(id, nombre) VALUES (125, 'Miguel Torrez');
+INSERT INTO estudiantes(id, nombre) VALUES (999, 'Sofia Pereira');
+INSERT INTO estudiantes(id, nombre) VALUES (2020, 'Ramses Alejandro');
+INSERT INTO estudiantes(id, nombre) VALUES (2121, 'Ramses');
+INSERT INTO estudiantes(id, nombre) VALUES (1212, 'Alejandro');
+
